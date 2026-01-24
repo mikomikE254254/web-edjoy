c60c1328987e45cb4a106fb40b3a2d69f9c0c21a
Miko Mike <mikomike2801111@gmail.com>
2026-01-24 23:00:23 +0000

    remove the revews all of them  (_for element <ProductReviews>_)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -9,7 +9,6 @@ import { cn } from '@/lib/utils';
 import { useCollection, useFirestore, useMemoFirebase } from '@/firebase';
 import { collection, query, where, limit } from 'firebase/firestore';
 import { Skeleton } from '@/components/ui/skeleton';
-import ProductReviews from '@/components/product/ProductReviews';
 
 type ImageType = Product['images'][0];
 
@@ -184,7 +183,6 @@ export default function ProductPage() {
           </div>
         </div>
       </div>
-      <ProductReviews />
     </>
   );
 }
