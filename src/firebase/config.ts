b3866d42370224a9553f85c77efc68f8302751bf
Miko Mike <mikomike2801111@gmail.com>
2026-01-24 17:44:56 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:4467006540:web:178939968363d13546e8e9",
   "apiKey": "AIzaSyAYRrkHLET9zbCNy0da_iAYQhSsl2SBuQ0",
   "authDomain": "studio-1140396316-71616.firebaseapp.com",
+  "storageBucket": "studio-1140396316-71616.appspot.com",
   "measurementId": "",
   "messagingSenderId": "4467006540"
 };
